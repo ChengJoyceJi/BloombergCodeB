cfe3c83de25ab1532123d18aeb17b0ac0f72c7b7
lynneeai <lynneeai@hotmail.com>
2015-03-21 01:35:25 -0400

    added bought and sold

--- stock.h
+++ stock.h
@@ -10,19 +10,33 @@ int curTime = 0;
 
 
 class Stock {
-	int id;
+	int ID;
 	std::string name;
+	std::vector<long double> myDiv;
+	int share;
+	
+	
 	std::vector<long double> netWorth;
 	std::vector<long double> bidPrice;
 	std::vector<long double> askPrice;
 	std::vector<long double> divRatio;
 	
 public:
-	Stock(std::string s) {name = s;}
+	Stock(std::string s, int x) {
+		name = s; 
+		ID = x;
+		share = 0;
+	}
+	
 	~Stock() {}
 	
-	int getID() {return id;}
+	int getID() {return ID;}
+	int getShare() {return share;}
+	void setShare(int x) {share = x;}
 	std::string getName() {return name;}
+	std::vector<long double> getmyDiv() {return myDiv;}
+	
+	
 	std::vector<long double> getnetWorth() {return netWorth;}
 	std::vector<long double> getBid() {return bidPrice;} 
 	std::vector<long double> getAsk() {return askPrice;}
@@ -30,6 +44,9 @@ public:
 	
 	void getInfo();
 	
+	void bought();
+	
+	void sold();
 };
 
 #endif
